064185fdf101fa1ff1b3b05a6c16b540993ab1a7
Lewis Barker <borkled@gmail.com>
2024-09-09 17:29:04 +0100

    Add tests for Game and WordleGame.__init__.py
    Using pytest and pytest-mock for unit tests since they're familiar and
    still seem to be among the best options.
    
    Some of these currently tests fail due to bugs or poorly defined
    behaviour identified by them. Some messing with PYTHONPATH is required
    to run the tests until the ability to install an editable package is set
    up.
    
    As always, trying to find the right balance between avoiding boilerplate
    noise in tests and avoiding making them too complex and hard to read -
    in my experience the best way to have tests that work correctly is to
    ensure that what they actually do is obvious. The mock_word_list fixture
    is pretty borderline in this regard, but it does make the test cases
    nice and simple.
    
    Testing it makes it more obvious that the way I've implemented ALPHABET
    as an abstract method is a bit weird, but it's the best way I've thought
    of so far that actually ensures it gets implemented by a subclass. The
    tests also make me think that the word list should probably be injected
    somehow, maybe I'll revisit that later.

---
+++ tests/test_wordall.py
@@ -0,0 +1,67 @@
+import pytest
+
+from wordall import wordall
+
+
+class TestIsWordInAlphabet:
+    @pytest.fixture
+    def game_with_alphabet(self, mocker):
+        # Patch Game so that we can directly create an instance without needing a
+        # subclass, and to set ALPHABET to a desired value
+        mocker.patch.multiple(wordall.Game, __abstractmethods__=set(), ALPHABET="abcde")
+        game = wordall.Game()
+        return game
+
+    def test_is_in_alphabet(self, game_with_alphabet):
+        assert game_with_alphabet.is_word_in_alphabet("baed")
+
+    def test_not_in_alphabet(self, game_with_alphabet):
+        assert not game_with_alphabet.is_word_in_alphabet("abf")
+
+
+class TestWordleGameInit:
+    @pytest.fixture
+    def mock_word_list(self, request, mocker):
+        word_list = request.param
+        word_list_data = "\n".join(word_list)
+        open_mock = mocker.patch(
+            "builtins.open", mocker.mock_open(read_data=word_list_data)
+        )
+        return open_mock, word_list
+
+    @pytest.mark.parametrize(
+        "mock_word_list", [["APPLE", "BREAD", "CHIPS"]], indirect=True
+    )
+    def test_loads_word_list(self, mock_word_list):
+        open_mock, word_list = mock_word_list
+        word_list_path = "/a/b/c"
+
+        wordle_game = wordall.WordleGame(word_list_path, 0)
+
+        open_mock.assert_called_once_with(word_list_path)
+        assert wordle_game.word_list == word_list
+
+    @pytest.mark.parametrize(
+        "mock_word_list", [["APPLE", "BREA8", "CHIPS"]], indirect=True
+    )
+    def test_exception_on_non_alphabet_word_list(self, mock_word_list):
+        with pytest.raises(wordall.InvalidWordListWordError):
+            wordall.WordleGame("/a/b/c", 0)
+
+    @pytest.mark.parametrize("mock_word_list", [[]], indirect=True)
+    def test_exception_on_empty_word_list(self, mock_word_list):
+        with pytest.raises(wordall.InvalidWordListError):
+            wordall.WordleGame("/a/b/c", 0)
+
+    @pytest.mark.parametrize(
+        "mock_word_list", [["APPLE", "BREAD", "CHIPS"]], indirect=True
+    )
+    def test_selects_random_target(self, mocker, mock_word_list):
+        _, word_list = mock_word_list
+
+        mock_choice = mocker.patch("random.choice")
+
+        wordle_game = wordall.WordleGame("/a/b/c", 0)
+
+        mock_choice.assert_called_once_with(word_list)
+        assert wordle_game.target == mock_choice.return_value
