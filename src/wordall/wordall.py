2c3f2dda79a026854a8095d5d0c3e5234419884a
Lewis Barker <borkled@gmail.com>
2024-09-09 13:48:40 +0100

    Add guess limit and failed game state

--- src/wordall/wordall.py
+++ src/wordall/wordall.py
@@ -1,6 +1,13 @@
 import abc
+import enum
 import random
 import string
+
+
+class GameState(enum.Enum):
+    GUESSING = 1
+    SUCCEEDED = 2
+    FAILED = 3
 
 
 class Game(abc.ABC):
@@ -9,6 +16,10 @@ class Game(abc.ABC):
     interaction. In general, will accept guesses until the correct word(s) is found, at
     which point the game ends.
     """
+
+    game_state: GameState
+
+    ALPHABET: str
 
     @property
     @abc.abstractmethod
@@ -25,7 +36,8 @@ class Game(abc.ABC):
             guess_word: A word to be guessed, possibly provided by a user.
         Returns:
             True if the game is "complete", False if the game is not complete. The game
-            is complete when the user has guessed the target word or words successfully.
+            is complete when the user has guessed the target word(s) successfully or run
+            out of guess attempts.
         """
         pass
 
@@ -42,11 +54,13 @@ class WordleGame(Game):
 
     ALPHABET = string.ascii_uppercase
 
-    def __init__(self, word_list_path: str) -> None:
+    def __init__(self, word_list_path: str, guess_limit: int) -> None:
         super().__init__()
         self.word_list = self._load_word_list(word_list_path)
         self.target = self._select_target()
         self.guesses = []
+        self.guess_limit = guess_limit
+        self.game_state = GameState.GUESSING
 
     def _load_word_list(self, word_list_path: str) -> list[str]:
         """
@@ -71,13 +85,22 @@ class WordleGame(Game):
         self.target = random.choice(self.word_list)
 
     def guess_word(self, guess_word: str) -> bool:
-        # Returns True if game is over
+        if self.game_state != GameState.GUESSING:
+            raise GameAlreadyFinishedError()
 
         if not self.is_word_in_alphabet:
             raise InvalidGuessWord(guess_word)
 
         self.guesses.append(guess_word)
-        return guess_word == self.target
+
+        if guess_word == self.target:
+            self.game_state = GameState.SUCCEEDED
+            return True
+        elif len(self.guesses) == self.guess_limit:
+            self.game_state = GameState.FAILED
+            return True
+        else:
+            return False
 
 
 class InvalidWordListWordError(Exception):
@@ -85,4 +108,8 @@ class InvalidWordListWordError(Exception):
 
 
 class InvalidGuessWord(Exception):
+    pass
+
+
+class GameAlreadyFinishedError(Exception):
     pass
